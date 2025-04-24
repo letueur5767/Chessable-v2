03bf3dffb21ed6703aa733d03d3a5e49ac1763f4
letueur5767 <115645202+letueur5767@users.noreply.github.com>
2025-04-24 09:32:42 +0200

    First Commit

---
+++ bugs.js
@@ -0,0 +1,11 @@
+//! Bugs
+
+//! --FIXED--  Player time stop on turn if opponent out of time.
+//! --FIXED--  Timeout function recheck.
+//! --FIXED--  Game stop after timeout (cant play moves after X).
+//! --FIXED--  Board flip stop button dosent turn time.
+
+//TODO New features
+
+// ? --DONE--  To make a Stop boardflip button
+// ? To make multiple themes for the board.
